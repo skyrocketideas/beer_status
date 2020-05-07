b7a3f1866ba78a2ae6f7d11e4a370e9c52f92798
Graeme Clark <grae0026@stud.kea.dk>
2020-05-07 08:46:26 +0200

    add svg

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 "use strict";
 import anime from "animejs/lib/anime.es.js";
+import { wrap } from "gsap";
 
 window.addEventListener("DOMContentLoaded", start);
 
@@ -17,19 +18,19 @@ function start() {
 	// console.log("start");
 	let visualizer = document.querySelector("#visualizer");
 	visualizer.style.setProperty("--ratio", 0);
-	anime();
+	// anime();
 }
 
 const looper = setInterval(() => {
 	getData();
 }, 5000);
 
-anime({
-	targets: "div",
-	opacity: [0, 1],
-	duration: 500,
-	delay: anime.stagger(300),
-});
+// anime({
+// 	targets: "div",
+// 	opacity: [0, 1],
+// 	duration: 500,
+// 	delay: anime.stagger(300),
+// });
 
 // get data from API
 async function getData() {
@@ -57,29 +58,41 @@ function makeObject(data) {
 function showObject(myObject) {
 	document.querySelector(".queue-number").textContent = myObject.inQueue;
 	// console.log(myObject.loggedAt);
+	let message = document.querySelector(".message");
+	let beer = document.querySelector(".beer");
+	let wrapper = document.querySelector(".wrapper");
 	let ratio = myObject.inQueue;
 	console.log(`ratio is ${ratio}`);
 	let visualizer = document.querySelector("#visualizer");
 	// visualizer.style.setProperty("--ratio", ratio);
 	// visualizer.style.width = ratio * 2 + "%";
 	visualizer.style.width = (ratio / 25) * 100 + "%";
-	if (ratio <= 5) {
+	if (ratio <= 7) {
 		console.log("Miller time!");
 		document.querySelector(".message").textContent = "Miller time!";
 		document.querySelector(".message").style.color = "white";
 		visualizer.style.backgroundColor = "white";
 		document.querySelector(".wrapper").style.backgroundColor = "var(--primary-color)";
-	} else if (ratio > 5 && ratio < 10) {
+		message.classList.toggle("fade");
+		beer.classList.add("rise");
+		wrapper.classList.add("fadeWrap");
+	} else if (ratio > 7 && ratio < 10) {
 		console.log("Wait a while ...");
 		visualizer.style.backgroundColor = "var(--secondary-color)";
 		document.querySelector(".message").textContent = "Hold your horses";
 		document.querySelector(".message").style.color = "var(--secondary-color)";
 		document.querySelector(".wrapper").style.backgroundColor = "white";
+		message.classList.toggle("fade");
+		beer.classList.remove("rise");
+		wrapper.classList.remove("fadeWrap");
 	} else if (ratio > 10) {
 		console.log("Leave it");
 		visualizer.style.backgroundColor = "var(--primary-color)";
 		document.querySelector(".message").textContent = "Don't bother";
 		document.querySelector(".message").style.color = "var(--primary-color)";
 		document.querySelector(".wrapper").style.backgroundColor = "white";
+		message.classList.toggle("fade");
+		beer.classList.remove("rise");
+		wrapper.classList.remove("fadeWrap");
 	}
 }
